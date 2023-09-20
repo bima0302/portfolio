87655db30df16249f0714fb3c5b682b405852a99
Bima Setyo <bima030201@gmail.com>
2023-09-20 19:29:24 +0700

    Update banner

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="bg-blue-100 mt-20 p-10 m-4 rounded-3xl">
       <div class="container mx-auto flex items-center justify-between">
-        <div class="text-2xl">Hello, I'm Bima!</div>
+        <div class="text-2xl">Hello, I&apos;m Bima!</div>
         <a
           href="https://www.linkedin.com/in/abimanyusrisetyo/"
           class="inline-block"
