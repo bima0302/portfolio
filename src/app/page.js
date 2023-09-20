ad3204ff34ea8a8884ec3a0b13630a97e8a0a786
Bima Setyo <bima030201@gmail.com>
2023-09-20 19:16:04 +0700

    Add banner

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-blue-100 mt-20 p-10 m-4 rounded-3xl">This is app</div>
+    <div className="bg-blue-100 mt-20 p-10 m-4 rounded-3xl">
+      <div class="container mx-auto flex items-center justify-between">
+        <div class="text-lg">This is your banner text</div>
+        <a
+          href="https://www.linkedin.com/in/abimanyusrisetyo/"
+          class="inline-block"
+          target="_blank"
+        >
+          <button class="bg-white text-blue-500 px-4 py-2 rounded-lg">
+            Contact Me
+          </button>
+        </a>
+      </div>
+    </div>
   );
 }
