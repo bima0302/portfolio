ffdd1cb00d337aa8bb9f0894f1edd7093c8e2bfa
Bima Setyo <bima030201@gmail.com>
2023-09-21 08:53:34 +0700

    Add hover effect on Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,18 +4,18 @@ function Card() {
   return (
     <div className="flex justify-center m-3 mt-20">
       <div className="w-96 mx-1">
-        <div className="bg-blue-100 h-64 p-6 rounded-3xl relative">
-          <div className="bg-blue-500 h-44 rounded-3xl absolute inset-0"></div>
+        <div className="bg-blue-100 hover:bg-blue-500 cursor-pointer h-64 p-6 rounded-3xl relative">
+          <div className="bg-blue-200 h-44 rounded-3xl absolute inset-0"></div>
         </div>
       </div>
       <div className="w-96 mx-1">
-        <div className="bg-blue-100 h-64 p-6 rounded-3xl relative">
-          <div className="bg-blue-500 h-44 rounded-3xl absolute inset-0"></div>
+        <div className="bg-blue-100 hover:bg-blue-500 cursor-pointer h-64 p-6 rounded-3xl relative">
+          <div className="bg-blue-200 h-44 rounded-3xl absolute inset-0"></div>
         </div>
       </div>
       <div className="w-96 mx-1">
-        <div className="bg-blue-100 h-64 p-6 rounded-3xl relative">
-          <div className="bg-blue-500 h-44 rounded-3xl absolute inset-0"></div>
+        <div className="bg-blue-100 hover:bg-blue-500 cursor-pointer h-64 p-6 rounded-3xl relative">
+          <div className="bg-blue-200 h-44 rounded-3xl absolute inset-0"></div>
         </div>
       </div>
     </div>
