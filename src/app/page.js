acf804211acd7bed54027ae161d0bd9469cc8dab
Bima Setyo <bima030201@gmail.com>
2023-09-24 14:49:02 +0700

    Remove Header :)

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Banner from "@/components/Banner";
 import Card from "@/components/Card";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Projects />
       <Card />
     </>
   );
