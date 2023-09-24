adb00c4c2d746a3e7be0e6e1491ba00928eee866
Bima Setyo <bima030201@gmail.com>
2023-09-24 13:33:16 +0700

    Add comments

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 function Banner() {
   return (
+    // TODO: Add banner
+    // TODO: Add background
     <div className="bg-blue-100 h-72 mt-20 p-10 m-4 rounded-3xl flex items-center">
+      {/* TODO: Add contents */}
       <div class="container mx-auto flex items-center justify-between">
+        {/* TODO: Add title */}
         <div class="text-2xl">Hello, I&apos;m Bima!</div>
+        {/* TODO: Add button (Contact Me) */}
         <a
           href="https://www.linkedin.com/in/abimanyusrisetyo/"
           class="inline-block"
