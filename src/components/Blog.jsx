3ee965aa019bdfb06b4a9ca80e5c6f5c7c89df94
Bima Setyo <bima030201@gmail.com>
2023-09-24 16:30:05 +0700

    Little fix

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Blog() {
   return (
-    <div className="text-center">
+    <div id="Blog" className="text-center">
       <h1 className="text-3xl font-bold mx-4 mt-20">My Blogs</h1> {/* Title */}
-      <div className="flex flex-col sm:flex-row items-center justify-center m-4">
+      <div className="flex flex-col md:flex-row items-center justify-center m-4">
         {/* Card 1 */}
         <div className="container bg-gray-50 hover:bg-blue-100 max-w-sm cursor-pointer h-72 rounded-3xl m-2">
-          <div className="bg-blue-200 h-4/6 w-90% rounded-3xl"></div>
+          <div className="bg-blue-200 h-4/6 rounded-3xl"></div>
           <div className="p-4">
             <div className="font-medium text-xl">Post 1</div>
             <p className="text-gray-700 text-base">
@@ -17,7 +17,7 @@ function Blog() {
         </div>
         {/* Card 2 */}
         <div className="container bg-gray-50 hover:bg-blue-100 max-w-sm cursor-pointer h-72 rounded-3xl m-2">
-          <div className="bg-blue-200 h-4/6 w-90% rounded-3xl"></div>
+          <div className="bg-blue-200 h-4/6 rounded-3xl"></div>
           <div className="p-4">
             <div className="font-medium text-xl">Post 2</div>
             <p className="text-gray-700 text-base">
@@ -27,7 +27,7 @@ function Blog() {
         </div>
         {/* Card 2 */}
         <div className="container bg-gray-50 hover:bg-blue-100 max-w-sm cursor-pointer h-72 rounded-3xl m-2">
-          <div className="bg-blue-200 h-4/6 w-90% rounded-3xl"></div>
+          <div className="bg-blue-200 h-4/6 rounded-3xl"></div>
           <div className="p-4">
             <div className="font-medium text-xl">Post 3</div>
             <p className="text-gray-700 text-base">
@@ -37,9 +37,11 @@ function Blog() {
         </div>
       </div>
       {/* See more button */}
-      <button className="mt-4 mx-auto px-4 py-2 w-72 text-blue-500 border border-blue-500 rounded-full hover:bg-blue-500 hover:text-white">
-        See more
-      </button>
+      <a href="http://blog.bimasetyo.com" target="_blank">
+        <button className="mt-4 mx-auto px-4 py-2 w-72 text-blue-500 border border-blue-500 rounded-full hover:bg-blue-500 hover:text-white">
+          See more
+        </button>
+      </a>
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 function Projects() {
   return (
-    <>
-      <div className="flex flex-col sm:flex-row items-center justify-center mt-20 mx-4">
+    <div id="Projects">
+      {/* Title */}
+      <h1 className="text-center text-3xl font-bold mx-4 mt-20">
+        My Projects
+      </h1>{" "}
+      <div className="flex flex-col md:flex-row items-center justify-center m-4">
         {/* TODO: Add Tumbnail */}
-        <div className="container bg-red-100 h-80 max-w-lg rounded-3xl hover:rounded-[48px]"></div>
+        <div className="container bg-red-100 h-80 max-w-xl rounded-3xl hover:rounded-[48px]"></div>
         {/* TODO: Add Project Details */}
-        <div className="container h-80 max-w-lg rounded-3xl p-8">
+        <div className="container h-80 max-w-xl rounded-3xl p-8">
           {/* TODO: Add title */}
           <div class="font-bold text-2xl">Lorem ipsum dolor.</div>
           {/* TODO: Add description */}
@@ -18,9 +22,9 @@ function Projects() {
           </p>
         </div>
       </div>
-      <div className="flex flex-col-reverse sm:flex-row items-center justify-center m-4">
+      <div className="flex flex-col-reverse md:flex-row items-center justify-center m-4">
         {/* TODO: Add Project Details */}
-        <div className="container h-80 max-w-lg rounded-3xl p-8">
+        <div className="container h-80 max-w-xl rounded-3xl p-8">
           {/* TODO: Add title */}
           <div class="font-bold text-2xl">Lorem ipsum dolor.</div>
           {/* TODO: Add description */}
@@ -31,9 +35,9 @@ function Projects() {
           </p>
         </div>
         {/* TODO: Add Tumbnail */}
-        <div className="container bg-red-100 h-80 max-w-lg rounded-3xl hover:rounded-[48px]"></div>
+        <div className="container bg-red-100 h-80 max-w-xl rounded-3xl hover:rounded-[48px]"></div>
       </div>
-    </>
+    </div>
   );
 }
 
