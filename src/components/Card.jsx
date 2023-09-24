c3b1f7944be2886aca4800ed203f4b0ff9e2e79a
Bima Setyo <bima030201@gmail.com>
2023-09-24 13:33:27 +0700

    Update Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,19 +3,19 @@ import React from "react";
 function Card() {
   return (
     <div className="flex justify-center m-3 mt-20">
+      {/* TODO: Add card */}
       <div className="w-96 mx-1">
-        <div className="bg-blue-100 hover:bg-blue-500 cursor-pointer h-64 p-6 rounded-3xl relative">
-          <div className="bg-blue-200 h-44 rounded-3xl absolute inset-0"></div>
-        </div>
-      </div>
-      <div className="w-96 mx-1">
-        <div className="bg-blue-100 hover:bg-blue-500 cursor-pointer h-64 p-6 rounded-3xl relative">
-          <div className="bg-blue-200 h-44 rounded-3xl absolute inset-0"></div>
-        </div>
-      </div>
-      <div className="w-96 mx-1">
-        <div className="bg-blue-100 hover:bg-blue-500 cursor-pointer h-64 p-6 rounded-3xl relative">
-          <div className="bg-blue-200 h-44 rounded-3xl absolute inset-0"></div>
+        {/* TODO: Add background */}
+        <div className="bg-gray-50 hover:bg-blue-100 cursor-pointer h-72 rounded-3xl">
+          {/* TODO: Add image */}
+          <div className="bg-blue-200 h-44 w-full rounded-3xl"></div>
+          {/* TODO: Add text */}
+          <div className="p-6">
+            {/* TODO: Add title */}
+            <div class="font-medium text-xl">Project 1</div>
+            {/* TODO: Add description */}
+            <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet.</p>
+          </div>
         </div>
       </div>
     </div>
