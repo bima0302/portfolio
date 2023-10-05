644905892c25517ade60a899488d8540f6f84259
Bima Setyo <bima030201@gmail.com>
2023-10-06 06:54:24 +0700

    Add Resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,16 @@ function Header() {
         <div class="font-bold md:text-6xl text-3xl">Hello, I&apos;m Bima!</div>
         {/* TODO: Add description */}
         <p class="font-base text-xl">Software engineer.</p>
+        {/* TODO: Add button (Resume) */}
+        <a
+          href="https://docs.google.com/document/d/1oz_eNx3OKB2aQUQJL6EOzYiw09i2EbShSsDUJjHa0dk/edit?usp=sharing"
+          class="inline-block"
+          target="_blank"
+        >
+          <button class="bg-white text-blue-500 px-4 py-2 rounded-3xl hover:bg-blue-500 hover:text-white m-2 transition duration-300">
+            Resume
+          </button>
+        </a>
         {/* TODO: Add button (Contact Me) */}
         <a
           href="https://www.linkedin.com/in/abimanyusrisetyo/"
@@ -27,10 +37,10 @@ function Header() {
             Projects
           </button>
         </a>
-        {/* TODO: Add button (Blog) */}
+        {/* TODO: Add button (Posts) */}
         <a href="#Blog" class="inline-block">
           <button class="bg-white text-blue-500 px-4 py-2 rounded-3xl hover:bg-blue-500 hover:text-white m-2 transition duration-300">
-            Blog
+            Posts
           </button>
         </a>
       </div>
