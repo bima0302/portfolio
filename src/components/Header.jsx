d391c476c3c70d7e5198dd8efdd3b6ada1eab73a
Bima Setyo <bima030201@gmail.com>
2023-11-11 09:22:54 +0700

    Hide header button in mobile mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,45 +4,47 @@ function Header() {
   return (
     // TODO: Add Header
     // TODO: Add background
-    <div className="bg-blue-100 md:h-96 h-80 m-4 rounded-3xl flex items-center justify-center text-center">
+    <div className='bg-blue-100 md:h-96 h-80 m-4 rounded-3xl flex items-center justify-center text-center'>
       {/* TODO: Add contents */}
-      <div class="container ">
+      <div class='container '>
         {/* TODO: Add title */}
-        <div class="font-bold md:text-6xl text-3xl">Hello, I&apos;m Bima!</div>
+        <div class='font-bold md:text-6xl text-3xl'>Hello, I&apos;m Bima!</div>
         {/* TODO: Add description */}
-        <p class="font-base text-xl">Software engineer.</p>
-        {/* TODO: Add button (Resume) */}
-        <a
-          href="https://docs.google.com/document/d/1oz_eNx3OKB2aQUQJL6EOzYiw09i2EbShSsDUJjHa0dk/edit?usp=sharing"
-          class="inline-block"
-          target="_blank"
-        >
-          <button class="bg-white text-blue-500 px-4 py-2 rounded-3xl hover:bg-blue-500 hover:text-white m-2 transition duration-300">
-            Resume
-          </button>
-        </a>
-        {/* TODO: Add button (Contact Me) */}
-        <a
-          href="https://www.linkedin.com/in/abimanyusrisetyo/"
-          class="inline-block"
-          target="_blank"
-        >
-          <button class="bg-white text-blue-500 px-4 py-2 rounded-3xl hover:bg-blue-500 hover:text-white m-2 transition duration-300">
-            Contact Me
-          </button>
-        </a>
-        {/* TODO: Add button (Projects) */}
-        <a href="#Projects" class="inline-block">
-          <button class="bg-white text-blue-500 px-4 py-2 rounded-3xl hover:bg-blue-500 hover:text-white m-2 transition duration-300">
-            Projects
-          </button>
-        </a>
-        {/* TODO: Add button (Posts) */}
-        <a href="#Blog" class="inline-block">
-          <button class="bg-white text-blue-500 px-4 py-2 rounded-3xl hover:bg-blue-500 hover:text-white m-2 transition duration-300">
-            Posts
-          </button>
-        </a>
+        <p class='font-base text-xl'>Software engineer.</p>
+        <div className='hidden md:block'>
+          {/* TODO: Add button (Resume) */}
+          <a
+            href='https://docs.google.com/document/d/1oz_eNx3OKB2aQUQJL6EOzYiw09i2EbShSsDUJjHa0dk/edit?usp=sharing'
+            class='inline-block'
+            target='_blank'
+          >
+            <button class='bg-white text-blue-500 px-4 py-2 rounded-3xl hover:bg-blue-500 hover:text-white m-2 transition duration-300'>
+              Resume
+            </button>
+          </a>
+          {/* TODO: Add button (Contact Me) */}
+          <a
+            href='https://www.linkedin.com/in/abimanyusrisetyo/'
+            class='inline-block'
+            target='_blank'
+          >
+            <button class='bg-white text-blue-500 px-4 py-2 rounded-3xl hover:bg-blue-500 hover:text-white m-2 transition duration-300'>
+              Contact Me
+            </button>
+          </a>
+          {/* TODO: Add button (Projects) */}
+          <a href='#Projects' class='inline-block'>
+            <button class='bg-white text-blue-500 px-4 py-2 rounded-3xl hover:bg-blue-500 hover:text-white m-2 transition duration-300'>
+              Projects
+            </button>
+          </a>
+          {/* TODO: Add button (Posts) */}
+          <a href='#Blog' class='inline-block'>
+            <button class='bg-white text-blue-500 px-4 py-2 rounded-3xl hover:bg-blue-500 hover:text-white m-2 transition duration-300'>
+              Posts
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
